b7db5189ddcc13879dcf6db08e58353adee7ed09
bsharabi <bsharabi95@gmail.com>
2025-12-10 13:54:43 +0200

    conf

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,11 +1,9 @@
 function App() {
-
-
   return (
-    <>
-    
-    </>
-  )
+    <div className="text-md text-red-200 m-9 p-7 border-amber-200 border-b-6">
+      Hello
+    </div>
+  );
 }
 
-export default App
+export default App;
