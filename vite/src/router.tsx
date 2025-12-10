68e7936446169103b002297efa5aee68db98b90c
bsharabi <bsharabi95@gmail.com>
2025-12-10 15:26:52 +0200

    build vite ptoject with tailwindcss, router,radix and axios

--- vite/src/router.tsx
+++ vite/src/router.tsx
@@ -7,7 +7,7 @@ import DashboardLayout from "./layouts/DashboardLayout";
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/dashboard" replace />,
+    element: <Navigate to="/auth/login" replace />,
   },
   {
     path: "auth",
